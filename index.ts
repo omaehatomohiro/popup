5a03e893dbdff507d0e8212a652e258c37a8f9a2
tomohiro <omaehatomohiro@gmail.com>
2022-07-05 17:03:28 +0900

    fixed

--- index.ts
+++ index.ts
@@ -1,49 +1,40 @@
-import Browser from './lib/Browser'
-import config from './lib/Config';
-import Popup from './lib/Popup';
-import './scss/style.scss';
-
+import Browser from "./lib/Browser";
+import config from "./config/Config";
+import Popup from "./lib/Popup";
+import "./scss/style.scss";
 
 main();
 
-function main(){
+function main() {
+  try {
+    if (Browser.isIE()) return false;
 
-    try{
-        if(Browser.isIE()) return false;
+    const popup = new Popup();
 
-        const popup = new Popup();
+    popup.init();
 
-        popup.init();
-
-        if(config.event.history){
-            popup.setHistoryEvent();
-        }
-
-        if(config.event.tabclose){
-           popup.setTabCloseEvent();
-        }
-
-        if(config.event.blur){
-            popup.setBlurEvent();
-        }
-
-        if(config.banner.popupType === 2){
-            popup.setScrollEvent();
-        }
-
-        popup.setTimerEvent();
-
-        popup.setRedirectEvent();
-
-        popup.setRemoveEvent();
-
-
-    }catch(e) {
-        console.log(e);
+    if (config.event.history) {
+      popup.setHistoryEvent();
     }
 
+    if (config.event.tabclose) {
+      popup.setTabCloseEvent();
+    }
+
+    if (config.event.blur) {
+      popup.setBlurEvent();
+    }
+
+    if (config.banner.popupType === 2) {
+      popup.setScrollEvent();
+    }
+
+    popup.setTimerEvent();
+
+    popup.setRedirectEvent();
+
+    popup.setRemoveEvent();
+  } catch (e) {
+    console.log(e);
+  }
 }
-
-
-
-
--- lib/Banner.ts
+++ lib/Banner.ts
@@ -1,13 +1,11 @@
-import config from './Config';
-import StorageHandler from './StorageHandler';
-
+import config from "../config/Config";
+import StorageHandler from "./StorageHandler";
 
 interface BannerInterface {
-  dom:HTMLDivElement;
-  closeBtn:HTMLDivElement;
+  dom: HTMLDivElement;
+  closeBtn: HTMLDivElement;
 
   popupType: number;
-
 
   /**
    * Close AB test
@@ -17,7 +15,7 @@ interface BannerInterface {
   /**
    * For ABテスト
    */
-  abType:string;
+  abType: string;
   typeBPath: string;
 
   /**
@@ -29,132 +27,128 @@ interface BannerInterface {
   pathC: string;
 }
 
-class Banner implements BannerInterface
-{
-    private static instance: Banner;
+class Banner implements BannerInterface {
+  private static instance: Banner;
 
-    dom: HTMLDivElement;
-    closeBtn: HTMLDivElement;
-    bannerImage: HTMLImageElement;
-    countDownDiv: HTMLDivElement;
-    popupType: number;
-    defaultPath: string;
-    abType:string;
-    typeBPath: string;
-    targetDomIdB: string;
-    targetDomIdC: string;
-    pathB: string;
-    pathC: string;
+  dom: HTMLDivElement;
+  closeBtn: HTMLDivElement;
+  bannerImage: HTMLImageElement;
+  countDownDiv: HTMLDivElement;
+  popupType: number;
+  defaultPath: string;
+  abType: string;
+  typeBPath: string;
+  targetDomIdB: string;
+  targetDomIdC: string;
+  pathB: string;
+  pathC: string;
 
-    constructor (){
-        this.dom = <HTMLDivElement>document.createElement('div');
-        this.closeBtn = <HTMLDivElement>document.createElement('div');
-        this.bannerImage = <HTMLImageElement>document.createElement('img');
-        this.countDownDiv =  <HTMLDivElement>document.createElement('div');
-        this.popupType = config.banner.popupType;
-        this.abType = StorageHandler.getAbType();
-        this.defaultPath = config.banner.defaultPath;
-        // this.typeAPath = config.banner.typeAPath;
-        this.typeBPath = config.banner.typeBPath;
-        this.targetDomIdB = config.banner.targetDomIdB;
-        this.targetDomIdC = config.banner.targetDomIdC;
-        this.pathB = config.banner.pathB;
-        this.pathC = config.banner.pathC;
+  constructor() {
+    this.dom = <HTMLDivElement>document.createElement("div");
+    this.closeBtn = <HTMLDivElement>document.createElement("div");
+    this.bannerImage = <HTMLImageElement>document.createElement("img");
+    this.countDownDiv = <HTMLDivElement>document.createElement("div");
+    this.popupType = config.banner.popupType;
+    this.abType = StorageHandler.getAbType();
+    this.defaultPath = config.banner.defaultPath;
+    // this.typeAPath = config.banner.typeAPath;
+    this.typeBPath = config.banner.typeBPath;
+    this.targetDomIdB = config.banner.targetDomIdB;
+    this.targetDomIdC = config.banner.targetDomIdC;
+    this.pathB = config.banner.pathB;
+    this.pathC = config.banner.pathC;
+  }
+
+  create(): void {
+    const container: HTMLDivElement = <HTMLDivElement>(
+      document.createElement("div")
+    );
+    container.id = "popupNormalContainer";
+    //container.className = 'active';
+    const inner = document.createElement("div");
+    const bannerWrap = document.createElement("div");
+    const closeBtn = document.createElement("div");
+    closeBtn.id = "popupCloseBtn";
+    closeBtn.className = "normalCloseBtn";
+    const bannerLink = document.createElement("div");
+    bannerLink.id = "popupBannerImgWrap";
+    bannerLink.className = "popupBannerLinkWrap";
+    const closeImage = document.createElement("img");
+    closeImage.src = "https://ad-pop.bbo-tech.com/images/close.png";
+    let bannerImage: any;
+
+    // ABテストの場合
+    if (this.popupType === 1) {
+      bannerImage = document.createElement("img");
+      bannerImage.className = "mainBannerImage";
+      bannerImage.src = this.selectAbImg();
+    } else {
+      bannerImage = document.createElement("img");
+      bannerImage.className = "mainBannerImage";
+      bannerImage.src = this.defaultPath; // close button Image path
     }
 
+    bannerImage.className = "mainBannerImage";
+    this.bannerImage = bannerImage;
 
-    create(): void{
-        const container:HTMLDivElement = <HTMLDivElement>document.createElement('div');
-        container.id = 'popupNormalContainer';
-        //container.className = 'active';
-        const inner = document.createElement('div');
-        const bannerWrap = document.createElement('div');
-        const closeBtn = document.createElement('div');
-        closeBtn.id = 'popupCloseBtn';
-        closeBtn.className = 'normalCloseBtn';
-        const bannerLink = document.createElement('div');
-        bannerLink.id = 'popupBannerImgWrap';
-        bannerLink.className = 'popupBannerLinkWrap';
-        const closeImage = document.createElement('img');
-        closeImage.src = 'https://ad-pop.bbo-tech.com/images/close.png';
-        let bannerImage:any;
+    // if (this.isCountdownEnable(this.timeMs)) {
+    const countDownDiv = document.createElement("div");
+    countDownDiv.id = "conterDiv";
+    countDownDiv.className = "countTimerWrap";
+    this.countDownDiv = countDownDiv;
+    bannerLink.appendChild(countDownDiv);
+    // }
 
-        // ABテストの場合
-        if(this.popupType === 1) {
-            bannerImage = document.createElement('img');
-            bannerImage.className = 'mainBannerImage';
-            bannerImage.src = this.selectAbImg();
-        }else{
-            bannerImage = document.createElement('img');
-            bannerImage.className = 'mainBannerImage';
-            bannerImage.src = this.defaultPath; // close button Image path
-        }
+    bannerLink.appendChild(bannerImage);
+    closeBtn.appendChild(closeImage);
+    bannerWrap.appendChild(closeBtn);
+    bannerWrap.appendChild(bannerLink);
+    inner.appendChild(bannerWrap);
+    container.appendChild(inner);
+    this.dom = container;
+    this.closeBtn = closeBtn;
+    // this.isExistFlag = true;
+    document.body.appendChild(container);
+  }
 
-        bannerImage.className = 'mainBannerImage';
-        this.bannerImage = bannerImage;
+  open(): void {
+    this.dom.classList.add("active");
+    console.log("open");
+  }
 
-      // if (this.isCountdownEnable(this.timeMs)) {
-        const countDownDiv = document.createElement('div');
-        countDownDiv.id = 'conterDiv';
-        countDownDiv.className = 'countTimerWrap';
-        this.countDownDiv = countDownDiv;
-        bannerLink.appendChild(countDownDiv);
-      // }
-    
-        bannerLink.appendChild(bannerImage);
-        closeBtn.appendChild(closeImage);
-        bannerWrap.appendChild(closeBtn);
-        bannerWrap.appendChild(bannerLink);
-        inner.appendChild(bannerWrap);
-        container.appendChild(inner);
-        this.dom = container;
-        this.closeBtn = closeBtn;
-        // this.isExistFlag = true;
-        document.body.appendChild(container);
+  remove(): void {
+    console.log("close");
+    this.dom.remove();
+  }
+
+  selectAbImg(): string {
+    if (this.abType === "verA") {
+      return this.defaultPath;
     }
-
-    open(): void {
-      this.dom.classList.add('active');
-      console.log('open')
+    if (this.abType === "verB") {
+      return this.typeBPath;
     }
+    return this.defaultPath;
+  }
 
-    remove(): void {
-      console.log('close');
-      this.dom.remove();
-    }
-  
+  imgSwitchToDefault() {
+    this.bannerImage.src = this.defaultPath;
+  }
 
-    selectAbImg(): string {
-        if(this.abType === 'verA') {
-          return this.defaultPath;
-        }
-        if(this.abType === 'verB') {
-          return this.typeBPath;
-        }
-        return this.defaultPath;
-    }
+  imgSwitchToB() {
+    this.bannerImage.src = this.pathB;
+  }
 
-    
-    imgSwitchToDefault(){
-        this.bannerImage.src = this.defaultPath;
-    }
+  imgSwitchToC() {
+    this.bannerImage.src = this.pathC;
+  }
 
-    imgSwitchToB(){
-        this.bannerImage.src = this.pathB;
+  public static getInstance() {
+    if (!Banner.instance) {
+      Banner.instance = new Banner();
     }
-
-    imgSwitchToC(){
-       this.bannerImage.src = this.pathC;
-    }
-
-    public static getInstance(){
-      if(!Banner.instance){
-        Banner.instance = new Banner();
-      }
-      return Banner.instance;
-    }
-    
+    return Banner.instance;
+  }
 }
-
 
 export default Banner;
